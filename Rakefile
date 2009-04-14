721b8bb9e559d6137eaa3219d4d558cc35b5cfee
Jerod Santo <jerod.santo@gmail.com>
2009-04-14 11:27:14 -0500

    namespaced inside Rack module and changed all names to noie6 for clarity

--- Rakefile
+++ Rakefile
@@ -3,6 +3,6 @@ require 'rake/testtask'
 task :default => [:test]
 
 Rake::TestTask.new do |task|
-  task.pattern = 'test/noie_test.rb'
+  task.pattern = 'test/noie6_test.rb'
   task.warning, task.verbose = true, true
 end
--- test/noie6_test.rb
+++ test/noie6_test.rb
@@ -3,7 +3,7 @@ require 'test/unit'
 require 'rubygems'
 require 'rack/mock'
 
-require File.join(File.dirname(__FILE__), '..', 'lib', 'noie')
+require File.join(File.dirname(__FILE__), '..', 'lib', 'noie6')
 
 class TestApp
   def call(env)
@@ -14,14 +14,14 @@ end
 class NoieTest < Test::Unit::TestCase
   
   def test_redirects_to_where_it_should_if_ie6
-    request = Rack::MockRequest.new(NoIE.new(TestApp.new, {:redirect => 'http://slashdot.org'}))
+    request = Rack::MockRequest.new(Rack::NoIE6.new(TestApp.new, {:redirect => 'http://slashdot.org'}))
     response = request.get('/', {'HTTP_USER_AGENT' => 'MSIE 6.0' })
     assert_equal 301, response.status
     assert_equal response.location, 'http://slashdot.org'
   end
   
   def test_allows_local_local_urls
-    request = Rack::MockRequest.new(NoIE.new(TestApp.new, {:redirect => '/foo'}))
+    request = Rack::MockRequest.new(Rack::NoIE6.new(TestApp.new, {:redirect => '/foo'}))
     response = request.get('/foo', {'HTTP_USER_AGENT' => 'MSIE 6.0' })    
     assert_equal "Hi Internets!", response.body
   end
